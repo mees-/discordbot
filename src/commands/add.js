50335c2533ce7bb0fd99055a2201601e9ebb8cb9
Mees van Dijk <mees@itisme.es>
2016-11-13 20:28:30 +0100

    added easter egg in command:add

--- src/commands/add.js
+++ src/commands/add.js
@@ -3,6 +3,9 @@ const log = require('debug')('bot:command:add')
 const { addedSong, errorAddedSong } = require('../messages')
 const testVoice = require('../music/testVoice')
 
+// for easter egg
+const ytdl = require('ytdl-core')
+
 module.exports = {
   name: 'add :url',
   usage: 'add <song url> [index in queue]',
@@ -13,6 +16,11 @@ module.exports = {
       .then((info) => {
         const manager = req.guild.voiceConnection.musicManager
         const song = new Song(req.params.get('url'), info, req.author, req.channel)
+        if (Math.random() <= 0.1) {
+          // easter egg, 1% chance to add rick astley - Never Gonna Give You Up
+          song.getStream = () =>
+            ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', { format: 'audioonly' })
+        }
         manager.addSong(song)
         res.end(addedSong(song.title))
       })
