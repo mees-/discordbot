a2def29fa4102edcb9d7deb6e83447968ca6e4af
Mees van Dijk <mees@itisme.es>
2016-11-14 15:15:48 +0100

    improvements for upcoming additions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 const Excord = require('excord')
-const router = require('./routes')
+const musicRouter = require('./musicRouter')
 
 const options = {
   prefix: '/' || process.env.BOT_PREFIX,
-  token: process.env.BOT_TOKEN
+  token: process.env.BOT_TOKEN,
+  channelId: '243026225268916224'
 }
 
 // init app
 const app = new Excord()
 
 app.use((req, res, next) => {
+  if (req.channel.id === options.channelId) {
+    next()
+  }
+  // if it isn't in the right channel, don't call next and cancel the chain
+})
+
+app.use((req, res, next) => {
   if (req.route.startsWith(options.prefix)) {
-    next(req.route.slice(options.prefix.length))
+    next(req.route.slice(options.prefix.length)) // remove the prefix from the route
   }
   // if it doesn't start with the prefix don't call next and just cancel the chain
 })
 
-app.use(router)
+// register routers
+app.use(musicRouter)
 
 app.login(options.token)
