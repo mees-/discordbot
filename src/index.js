b8c70cfae45d03b7c7aa7c33b18fc573a94195f7
Mees van Dijk <mees@itisme.es>
2016-11-15 22:36:25 +0100

    changed from hardcoded to env options

--- src/index.js
+++ src/index.js
@@ -2,26 +2,29 @@ const Excord = require('excord')
 const musicRouter = require('./musicRouter')
 
 const options = {
-  prefix: '/' || process.env.BOT_PREFIX,
+  prefix: process.env.BOT_PREFIX || '/',
   token: process.env.BOT_TOKEN,
-  channelId: '243026225268916224'
+  channelId: process.env.BOT_CHANNEL
 }
 
 // init app
 const app = new Excord()
 
+// filter based on channel
 app.use((req, res, next) => {
+  if (!options.channelId) return next()
   if (req.channel.id === options.channelId) {
     next()
   }
   // if it isn't in the right channel, don't call next and cancel the chain
 })
 
+// filter based on prefix
 app.use((req, res, next) => {
   if (req.route.startsWith(options.prefix)) {
     next(req.route.slice(options.prefix.length)) // remove the prefix from the route
   }
-  // if it doesn't start with the prefix don't call next and just cancel the chain
+  // if it doesn't start with the prefix don't call next and cancel the chain
 })
 
 // register routers
