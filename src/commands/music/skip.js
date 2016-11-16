8009177da308e9d5f88656fb5c42ec54acfc3f1b
Mees van Dijk <mees@itisme.es>
2016-11-16 21:41:09 +0100

    added skipamount to skip command

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -2,14 +2,17 @@ const testVoice = require('../../music/testVoice')
 const log = require('debug')('bot:command:skip')
 
 module.exports = {
-  name: 'skip',
+  name: 'skip ?amount',
   usage: 'skip [amount]',
 
   run(req) {
     if (!testVoice(req)) return
-
+    let amount = 1
+    try {
+      amount = parseInt(req.params.get('amount'))
+    } catch (e) {}
     const manager = req.guild.voiceConnection.musicManager
-    manager.next(1)
+    manager.next(amount)
     if (manager.queue[0]) {
       manager.start()
     } else {
