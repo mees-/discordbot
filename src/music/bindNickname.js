84663d36a992af382e7243304b1422558ab9aef3
Mees van Dijk <mees@itisme.es>
2016-11-19 13:00:01 +0100

    removed timeouts

--- src/music/bindNickname.js
+++ src/music/bindNickname.js
@@ -4,12 +4,7 @@ module.exports = function bindMusicToNickName(guild, manager) {
   const ownGuildMember = guild.members.find('id', guild.client.user.id)
   const originalNickName = process.env.BOT_NICK || ownGuildMember.nickname
 
-  let timeout = true
   manager.on('start', (song) => {
-    debugger
-    if (timeout) {
-      timeout = false
-    }
     ownGuildMember.setNickname(`- ${ song.shortTitle }`)
     .catch((e) => {
       debug('error when setting nickname', e)
@@ -17,15 +12,9 @@ module.exports = function bindMusicToNickName(guild, manager) {
   })
 
   manager.on('song end', () => {
-    timeout = setTimeout(() => {
-      if (timeout) {
-        ownGuildMember.setNickname(originalNickName)
-          .catch((e) => {
-            debug('error when setting nickname', e)
-          })
-      } else {
-        timeout = true
-      }
-    }, 1000)
+    ownGuildMember.setNickname(originalNickName)
+      .catch((e) => {
+        debug('error when setting nickname', e)
+      })
   })
 }
