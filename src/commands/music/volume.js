d70e020f62a15485fec8b2bafc7dcd87bedd4a5b
Mees van Dijk <mees@itisme.es>
2016-11-21 14:34:18 +0100

    cleaned up volume command

--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -3,20 +3,20 @@ const testVoice = require('../../music/testVoice')
 const log = require('debug')('corldlr-music:command:volume')
 
 module.exports = {
-  name: 'volume :percent',
-  usage: 'volume <percentage> [method](log | db | scale)',
+  name: 'volume :vol',
+  usage: 'volume <percentage>',
 
   run(req, res) {
     if (!testVoice(req)) return
-    if (req.params.get('percent') >= 300) {
+    const newVolume = req.params.get('vol') / 100
+    if (newVolume >= 300) {
       log('Not allowing volumes over 300, people hate it.')
       return res.end(tooHighVolume())
     }
     const manager = req.guild.voiceConnection.musicManager
     const oldVolume = manager.volume
-    const newVolume = req.params.get('percent') / 100
-    log(`setting volume to ${ newVolume }% with setting 'log'`)
+    log(`setting volume to ${ newVolume * 100 }%`)
     manager.volume = newVolume
-    res.end(changeVolume(oldVolume < newVolume))
+    res.end(changeVolume(oldVolume > newVolume))
   }
 }
