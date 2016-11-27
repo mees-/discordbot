85a13b7507cca1d3aa6b206db8317184c3916c3e
Mees van Dijk <mees@itisme.es>
2016-11-27 04:20:03 +0100

    Added clicking songs to skip to them

--- src/web/createApp.js
+++ src/web/createApp.js
@@ -32,11 +32,11 @@ module.exports = function createServer(bot) {
     if (!guild.voiceConnection) return res.end('No voiceConnection')
     // find manager
     const manager = guild.voiceConnection.musicManager
-    if (!manager) return res.end('No musicmanager, let the bot join through music command')
+    if (!manager) return res.end('No musicmanager, let the bot join through the music command')
 
     const locals = {
       pageTitle: `dashboard - ${ guild.name }`,
-      songs: manager.queue.slice(1)
+      songs: manager.queue.map((value, index) => Object.assign({}, value, { index, url: `/${ guild.id }/skip/${ index }` })).slice(1)
     }
     if (manager.queue[0]) {
       locals.current = `Currently playing: ${ manager.queue[0].title }`
@@ -44,6 +44,28 @@ module.exports = function createServer(bot) {
       locals.current = 'Currently playing: nothing'
     }
     res.render('dash', locals)
+  })
+  server.get('/:guild/skip/:index', (req, res) => {
+    // find guild
+    const guild = bot.guilds.find('id', req.params.guild)
+    if (!guild) return res.end('No such guild')
+
+    // test for voice
+    if (!guild.voiceConnection) return res.end('No voiceConnection')
+    // find manager
+    const manager = guild.voiceConnection.musicManager
+    if (!manager) return res.end('No musicmanager, let the bot join through the music command')
+
+    const parsedIndex = parseInt(req.params.index)
+    if (isNaN(parsedIndex)) {
+      return res.status(404).end()
+    }
+
+    manager.forceStop()
+    manager.queue = manager.queue.slice(parsedIndex)
+    manager.start()
+
+    res.redirect(`/${ req.params.guild }`)
   })
 
   return server
